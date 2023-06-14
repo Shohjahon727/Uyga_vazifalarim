b19f5bbf377b66f541549ccd8cd2c6132f730e5e
Shohjahon727 <135617343+Shohjahon727@users.noreply.github.com>
2023-06-14 17:09:24 +0500

    Learning Class

---
+++ MyClass/Kitob.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class Kitob
+{
+	public string Name;
+	public string Author;
+	public int Pages;
+	public string Genre;
+
+	public Kitob() { }
+	public Kitob (string name,string author,int pages,string genre)
+	{
+		Name = name;
+		Author = author;
+		Pages = pages;
+		Genre = genre;
+
+	}
+
+	public void PrintInfo()
+	{
+        Console.WriteLine($"Name : {Name} \n" +
+			$"Author : {Author}\n" +
+			$"Pages : {Pages}\n" +
+			$"Genre : {Genre}\n");
+    }
+}
+
+
---
+++ MyClass/Program.cs
@@ -0,0 +1,5 @@
+﻿
+var Book = new Kitob("Garry Potr","Potr",400,"Advanture");
+Book.PrintInfo();
+Console.ReadKey();
+
